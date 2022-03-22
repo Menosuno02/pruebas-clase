906cf9b08f7d9f9f0bfdd39ed71dc52d021d6bf4
Menosuno (Alejandro López) <mainusone@gmail.com>
2022-03-22 08:37:00 +0100

    Creación programa

---
+++ src/git/GIT.java
@@ -0,0 +1,16 @@
+package git;
+
+/**
+ *
+ * @author administrador
+ */
+public class GIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba GIT");
+    }
+
+}

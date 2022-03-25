edb261bd5877497b2ae0d704f642d0f543d9a5e9
Menosuno (Alejandro López) <mainusone@gmail.com>
2022-03-25 14:26:01 +0100

    Added class Nada

---
+++ src/git/Nada.java
@@ -0,0 +1,23 @@
+package git;
+
+/**
+ *
+ * @author administrador
+ */
+public class Nada {
+
+    private int num;
+
+    public Nada(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int x) {
+        this.num = this.num + x;
+    }
+
+}
